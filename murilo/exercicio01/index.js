2ac061592355ecf677594af230e72f282b9a13ef
Murilo <murilosilveira.pl017@academico.ifsul.edu.br>
2025-08-18 22:27:07 -0300

    first commit

---
+++ murilo/exercicio01/index.js
@@ -0,0 +1,4 @@
+import FunctionString from './modules/strings.js'
+
+console.log('Exercício 01 - FunctionStrings');
+FunctionString();
---
+++ murilo/exercicio01/modules/strings.js
@@ -0,0 +1,15 @@
+export default function() {
+
+  const texto1 = "ola ";
+  const texto2 = 'mundo';
+  const mensagem = String(texto1 + texto2);
+  
+  console.log('1' + 1)
+  console.log(texto1 + texto2)
+  
+  console.log(`O programa diz: ${mensagem}`);
+  console.log(`O valor de PI é: ${Math.PI.toFixed(2)}`);
+
+
+
+}  
