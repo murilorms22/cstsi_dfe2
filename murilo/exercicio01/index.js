a65ac5ecc5a0b151cc366046dc7e2b73e2cc3c3a
Murilo <murilosilveira.pl017@academico.ifsul.edu.br>
2025-08-25 21:29:01 -0300

    functions: carro, frutas, numeros. uso de push, pop, shift, unshift, splice, sort, reverse, foreach

--- murilo/exercicio01/index.js
+++ murilo/exercicio01/index.js
@@ -1,4 +1,8 @@
+import funcoes from './modules/functions.js';
 import FunctionString from './modules/strings.js'
 
-console.log('Exercício 01 - FunctionStrings');
+console.log('Exercício 01 - Strings');
 FunctionString();
+
+console.log('\nExercício 02 - Functions');
+funcoes();
---
+++ murilo/exercicio01/modules/functions.js
@@ -0,0 +1,36 @@
+export default function() {
+
+const carros = ['gol', 'palio', 'celta'];
+let frutas = Array("Laranja", "Maçã", "Uva");
+let nuns = Array("Laranja", "Maçã", "Uva");
+
+console.log(`Carros: ${carros} \nFrutas: ${frutas}`);
+
+carros.push('clio');
+let carroRetirado = carros.shift();
+console.log(`Carro retirado: ${carroRetirado}\n\n Novo array carros: ${carros}`);
+
+let frutaRetirada = frutas.pop();
+frutas.unshift('Limão')
+console.log(`Fruta retirada: ${frutaRetirada}\n\n Novo array frutas: ${frutas}`);
+
+carros.splice(2, 1, "corola", "onix");
+console.log('Splice feito. Novo array carros: ' + carros);
+
+let numeros = [10, 30, 20, 15, 50, 45];
+console.log(`Numeros: ${numeros}`);
+
+let numerosOrd = numeros.sort((a,b) => b-a);
+console.log(`NumerosOrd: ${numerosOrd}`);
+
+let numerosOrdRev = numerosOrd.reverse();
+console.log(`NumerosOrdRev: ${numerosOrdRev}`);
+
+console.log('\nExercício 03 - forEach');
+
+carros.forEach(imprimeCarros);
+function imprimeCarros(car) {
+  console.log(car)
+}
+
+}
